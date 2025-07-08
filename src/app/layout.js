035b7adac7b4fd70a7f7ae97bd67a7c918c855d4
Dax-savani <dakshsavani15@gmail.com>
2025-07-08 14:52:43 +0530

    chatbot added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@mui/material";
 import theme from "../components/theme/theme";
 import Navigation from "../components/global/navigation";
 import Footer2 from "../components/global/footer2";
+import Script from "next/script";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -32,6 +33,23 @@ export default function RootLayout({ children }) {
         <ThemeProvider theme={theme}>
             <Navigation />
             {children}
+            <Script
+                id="tawkto"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/686cdcbcf905731911104cee/1ivkjsgka';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `
+                }}
+            />
             <Footer2 />
         </ThemeProvider>
         </body>
