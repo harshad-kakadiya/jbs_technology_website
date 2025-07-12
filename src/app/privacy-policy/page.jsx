bf2d63d294eb9b0d72835e32fddf20f1449e30e1
harshad-kakadiya <harshadkakadiya0096@gmail.com>
2025-07-12 18:01:32 +0530

    blog data change , privacy policy page complated

---
+++ src/app/privacy-policy/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Privacy from "@/components/privacy-policy/privacy";
+
+function Page() {
+    return (
+        <>
+            <Privacy/>
+        </>
+    );
+}
+
+export default Page;
