94f8b49ef46ee16eb42f6b2fa22e62770ec7de0a
harshad-kakadiya <harshadkakadiya0096@gmail.com>
2025-07-12 13:40:36 +0530

    some changes

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Box} from "@mui/material";
 import DevlopTogether from "@/components/singleCapability/devlopTogether";
+import Faqs from "@/components/singleCapability/faqs";
 
 function Page() {
     return (
         <>
          <DevlopTogether/>
+            <Faqs/>
         </>
     );
 }
